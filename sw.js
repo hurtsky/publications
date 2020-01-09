d2b41f9ce83304d6ef6adfd3f0730e3b9d8a3f83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-09 20:18:41 +0000

    Deploy SEAMEO-SEN/publications to github.com/SEAMEO-SEN/publications.git:master

--- sw.js
+++ sw.js
@@ -33,14 +33,14 @@ self.__precacheManifest = [
     "url": "commons-4898868cdb38862b273a.js"
   },
   {
-    "url": "app-6110c7fb4c55cbf94be7.js"
+    "url": "app-987f232f75ee8f2f7e40.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-95b4e8755293630be937.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "025860054a5f8c120cebc732fd01f5f5"
+    "revision": "9bc776782e3d70a6ef53c86d0bb1a266"
   },
   {
     "url": "manifest.webmanifest",
@@ -137,7 +137,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-6110c7fb4c55cbf94be7.js`))) {
+  if (!resources || !(await caches.match(`/app-987f232f75ee8f2f7e40.js`))) {
     return await fetch(event.request)
   }
 
