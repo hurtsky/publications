3e7b2772ab931ba6f8fa25591269b98ad24f9f22
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-31 03:30:10 +0000

    Deploy SEAMEO-SEN/publications to github.com/SEAMEO-SEN/publications.git:master

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eccd2ad987a6cb55d8e2ca9eee9d80dd"
+    "revision": "3e0f8261a11b841fce63016b05a434aa"
   },
   {
     "url": "static/webfonts/s/robotoslab/v11/BngbUXZYTXPIvIBgJJSb6s3BzlRRfKOFbvjojISmb2Rj.woff2"
